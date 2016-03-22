ba7fc05b110a2c6e754d30e43f7f619e63effafb
Alex Lakatos <alex.lakatos@ovoenergy.com>
2016-03-22 19:43:34 +0000

    more cleanup and adding some style

---
+++ src/slides.js
@@ -0,0 +1,7 @@
+export class Slides {
+  slides = [{
+      avatar_url: "http://alexlakatos.github.io/slides/codecamp/pictures/campuslon/be-the-future.jpg",
+      login: "shits",
+      html_url: "http://alexlakatos.github.io/slides/codecamp/pictures/campuslon/be-the-future.jpg"
+  }];
+}
