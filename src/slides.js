8ec90569c8a194e29287e84fa36e5c3f6eb5b4e5
Alex Lakatos <alex.lakatos@ovoenergy.com>
2016-03-29 20:21:19 +0100

    pushing daisies. or incomplete slides

--- src/slides.js
+++ src/slides.js
@@ -1,7 +1,120 @@
 export class Slides {
-  slides = [{
-      avatar_url: "http://alexlakatos.github.io/slides/codecamp/pictures/campuslon/be-the-future.jpg",
-      login: "shits",
-      html_url: "http://alexlakatos.github.io/slides/codecamp/pictures/campuslon/be-the-future.jpg"
-  }];
+    slides = [{
+        cover_url: "/assets/codecamp-iasi.png",
+        title: "Web Performance 101",
+        slide_url: "http://alexlakatos.github.io/slides/codecamp/",
+        conference: {
+            name: "CodeCamp Iasi",
+            link: "http://iasi.codecamp.ro/events/CodeCamp12oct2013/index.html",
+        },
+        presentation_date: "12-10-2013"
+    }, {
+        cover_url: "/assets/developer-day-montenegro-1.png",
+        title: "Firefox OS - Beginning of something big",
+        slide_url: "http://alexlakatos.github.io/slides/developers-day-montenegro-part1/",
+        conference: {
+            name: "Developers Day",
+            link: "https://www.telenor.me/en/About-Telenor/Media-Center/Press-Releases-2013/Developers-Day-workshop-for-developers/?pn=3",
+        },
+        presentation_date: "09-06-2013"
+    }, {
+        cover_url: "/assets/developer-day-montenegro-2.png",
+        title: "WebAPIs... and more...",
+        slide_url: "http://alexlakatos.github.io/slides/developers-day-montenegro-part2/",
+        conference: {
+            name: "Developers Day",
+            link: "https://www.telenor.me/en/About-Telenor/Media-Center/Press-Releases-2013/Developers-Day-workshop-for-developers/?pn=3",
+        },
+        presentation_date: "09-06-2013"
+    }, {
+        cover_url: "/assets/electrotech-2013.png",
+        title: "Firefox OS - formerly known as Boot to Gecko",
+        slide_url: "http://alexlakatos.github.io/slides/electrotech-aprilie-2013/",
+        conference: {
+            name: "Electrotech",
+            link: "http://electrotech.osut.org/",
+        },
+        presentation_date: "08-04-2013"
+    }, {
+        cover_url: "/assets/firefox-night.png",
+        title: "Firefox OS - Beginning of something big",
+        slide_url: "http://alexlakatos.github.io/slides/firefox-night/",
+        conference: {
+            name: "Firefox Night",
+            link: "http://alexlakatos.github.io/slides/firefox-night/",
+        },
+        presentation_date: "28-05-2013"
+    }, {
+        cover_url: "/assets/gamification.png",
+        title: "Gamification",
+        slide_url: "http://alexlakatos.github.io/slides/gamification/",
+        conference: {
+            name: "EBS Training",
+            link: "http://alexlakatos.github.io/slides/gamification/",
+        },
+        presentation_date: "04-04-2014"
+    }, {
+        cover_url: "/assets/geekmeet-17.png",
+        title: "Firefox OS - formerly known as Boot to Gecko",
+        slide_url: "http://alexlakatos.github.io/slides/geekmeet-17/",
+        conference: {
+            name: "GeekMeet 17",
+            link: "http://geekmeet.ro/cluj/2013/05/15/geekmeet-17-cluj/",
+        },
+        presentation_date: "25-05-2013"
+    }, {
+        cover_url: "/assets/git-basic.png",
+        title: "Git Basic Training",
+        slide_url: "http://alexlakatos.github.io/slides/git-basic-training/",
+        conference: {
+            name: "EBS Training",
+            link: "http://alexlakatos.github.io/slides/git-basic-training/",
+        },
+        presentation_date: "04-02-2014"
+    }, {
+        cover_url: "/assets/mobile-monday-cluj.png",
+        title: "Firefox OS",
+        slide_url: "http://alexlakatos.github.io/slides/mobile-monday-cluj/",
+        conference: {
+            name: "Mobile Monday Cluj",
+            link: "http://www.mobilemonday.ro/",
+        },
+        presentation_date: "30-09-2013"
+    }, {
+        cover_url: "/assets/mobile-monday-serbia.png",
+        title: "Firefox OS - formerly known as Boot to Gecko",
+        slide_url: "http://alexlakatos.github.io/slides/mobile-monday-serbia/",
+        conference: {
+            name: "Mobile Monday Serbia",
+            link: "http://www.momo.rs/",
+        },
+        presentation_date: "27-05-2013"
+    }, {
+        cover_url: "/assets/motivating.png",
+        title: "Motivating the trainees",
+        slide_url: "http://alexlakatos.github.io/slides/motivating-your-trainees/",
+        conference: {
+            name: "EBS Training",
+            link: "http://alexlakatos.github.io/slides/motivating-your-trainees/",
+        },
+        presentation_date: "26-03-2014"
+    }, {
+        cover_url: "/assets/open-source-iasi.png",
+        title: "Developing for Firefox OS",
+        slide_url: "http://alexlakatos.github.io/slides/open-source-iasi/",
+        conference: {
+            name: "Open Source Iasi",
+            link: "https://opensourceiasi.wordpress.com/",
+        },
+        presentation_date: "15-03-2014"
+    }, {
+        cover_url: "/assets/open-source-testing.png",
+        title: "Open Source Testing",
+        slide_url: "http://alexlakatos.github.io/slides/open-source-testing/",
+        conference: {
+            name: "Tabara de Testare",
+            link: "http://tabaradetestare.ro/",
+        },
+        presentation_date: "06-02-2012"
+    }];
 }
