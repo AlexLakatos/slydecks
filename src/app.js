282748e5fe889cf0e94e3f1328f98a3aea6075b3
Alex Lakatos <alex.lakatos@ovoenergy.com>
2016-04-14 18:23:43 +0100

    Revert "recommit"
    This reverts commit 82c456f60fb96f2e1936adc96de364e43f158c7d.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Alex Lakatos';
+
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', "slides"],         name: 'slides',        moduleId: 'slides',        nav: true, title: 'Slydecks' }
     ]);
+
+    router.title_link = "http://alexlakatos.github.io/";
 
     this.router = router;
   }
